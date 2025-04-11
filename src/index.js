8bcfd0829c7018f0d54bb5e0f526c4d6fc664626
Sarra <Sarra.rajichi@gmail.com>
2025-04-11 10:26:47 +0200

    Ajout d'une fonction utilitaire de formatage de date

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ function greet(name) {
 module.exports = { greet };
 const { login } = require('./auth/login');
 module.exports.auth = { login };
+// Gestionnaire d'erreurs global
+function handleError(error) {
+  console.error('Error:', error.message);
+}
+module.exports.handleError = handleError;
