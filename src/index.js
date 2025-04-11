af4fbdb15addf986b56a7ca659526a5f995f4cb0
Sarra <Sarra.rajichi@gmail.com>
2025-04-11 10:28:15 +0200

    Ajout d'une fonction utilitaire de formatage de date

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ function greet(name) {
 module.exports = { greet };
 const { login } = require('./auth/login');
 module.exports.auth = { login };
+// Utilitaires
+function formatDate(date) {
+  return date.toISOString();
+}
+module.exports.utils = { formatDate };
